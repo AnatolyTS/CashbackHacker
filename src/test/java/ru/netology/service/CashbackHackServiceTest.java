e0f6a2f2e32229cbb8888432e98dd21ec96b3a84
Anatoly Tsybrov <ttoljke@gmail.com>
2021-01-12 01:54:43 +0300

    testng add

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,41 @@
+package ru.netology.service;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+
+public class CashbackHackServiceTest {
+
+    @Test
+    public void shouldReturn100IfAmountLowerThan1000() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 900;
+
+        int actual = service.remain(amount);
+        int expected = 100;
+
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldReturnIfAmountGreaterThan1000() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1500;
+
+        int actual = service.remain(amount);
+        int expected = 500;
+
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldReturn0IfAmount1000() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+
+        int actual = service.remain(amount);
+        int expected = 0;
+
+        Assert.assertEquals(actual, expected);
+    }
+}
